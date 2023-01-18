6e1652614b68d081fff5187d7bfd1c7cbf0716ca
drelocatelli <andressalocatelli@hotmail.com>
2023-01-18 12:26:08 -0300

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        try {
+            $validateUser = Validator::make($request->all(), [
+                'name' => 'required',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required'
+            ]);
+
+            if($validateUser->fails()) {
+                return response()->json([
+                    'message' => 'Ocorreu um erro ao se registrar',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+            $user = User::create([
+                'name' => $request->get('name'),
+                'email' => $request->get('email'),
+                'password' => Hash::make($request->get('password'))
+            ]);
+
+            return response()->json([
+                'message' => 'Registro realizado com sucesso!',
+                'token' => $user->createToken('API TOKEN')->plainTextToken
+            ], 201);
+            
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function login(Request $request) 
+    {
+        
+        try {
+            $validateUser = Validator::make($request->all(), [
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+    
+            if($validateUser->fails()) {
+                return response()->json([
+                    'message' => 'Não foi possível fazer login',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+            $attemp = !(Auth::attempt($request->only(['email', 'password']), false, false));
+    
+            if($attemp) {
+                return response()->json([
+                    'message' => 'E-mail e senha não conferem'
+                ], 401);
+            }
+    
+            $user = User::where('email', $request->get('email'))->first();
+            return response()->json([
+                'message' => 'Usuário autenticado',
+                'token' => $user->createToken('API TOKEN')->plainTextToken
+            ], 200);
+        } catch(\Throwable $th) {
+            return response()->json([
+                'message' => $th->getMessage()
+            ], 500);
+        }
+        
+    }
+    
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+/**
+ *  USER AUTHENTICATION
+ * 
+ */
+Route::controller(AuthController::class)->prefix('/auth')->group(function() {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+});
